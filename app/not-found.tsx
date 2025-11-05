8f8c95d0fc2d95500dd4e250715397c78e96968f
ardianilyas <ardianilyas@gmail.com>
2025-11-05 22:51:36 +0700

    feat(ui): add custom 404 page, will refactor soon

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NotFoundPage() {
+    return (
+        <div className="flex flex-col min-h-screen items-center justify-center bg-fuchsia-50 font-sans dark:bg-black">
+            <h1 className="text-3xl font-medium">404 - Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+            <Link href="/">Go Home</Link>
+        </div>
+    )
+}
