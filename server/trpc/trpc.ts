22c2a288ba3031631bf15f4ff482db4483eee154
ardianilyas <ardianilyas@gmail.com>
2025-11-08 17:02:06 +0700

    feat: add auth and admin procedure

--- server/trpc/trpc.ts
+++ server/trpc/trpc.ts
@@ -1,7 +1,8 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { Context } from "./context";
 import superjson from "superjson";
-import { ZodError } from "zod";
+import z, { ZodError } from "zod";
+import { Role } from "@/lib/generated/prisma/enums";
 
 export const t = initTRPC.context<Context>().create({
     transformer: superjson,
@@ -9,10 +10,37 @@ export const t = initTRPC.context<Context>().create({
         return {
             ...shape,
             message: error.message,
-            zodError: error.cause instanceof ZodError ? error.cause.flatten() : null,
+            zodError: error.cause instanceof ZodError ? z.treeifyError(error.cause) : null,
         };
     },
 });
 
+const isAuthed = t.middleware(({ ctx, next }) => {
+    if(!ctx.session || !ctx.session.user) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+
+    return next({
+        ctx: {
+            user: ctx.session.user,
+        },
+    });
+});
+
+const isAdmin = t.middleware(({ ctx, next }) => {
+    if(ctx.session?.user.role !== Role.ADMIN) {
+        throw new TRPCError({ code: "FORBIDDEN", message: "You do not have permission to access this resource." });
+    }
+
+    return next({
+        ctx: {
+            user: ctx.session.user,
+        },
+    });
+});
+
 export const router = t.router;
 export const publicProcedure = t.procedure;
+
+export const authProcedure = t.procedure.use(isAuthed);
+export const adminProcedure = authProcedure.use(isAdmin);
