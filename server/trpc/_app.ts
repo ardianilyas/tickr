e614dafc8e01e3f136eef06b29bce55d600f7aff
ardianilyas <ardianilyas@gmail.com>
2025-11-09 19:38:45 +0700

    feat: add admin ticket router

--- server/trpc/_app.ts
+++ server/trpc/_app.ts
@@ -1,8 +1,10 @@
 import { adminCategoriesRouter } from "@/features/admin/categories/routers/adminCategoriesRouter";
 import { router } from "./trpc";
+import { adminTicketRouter } from "@/features/admin/tickets/routers/adminTicketRouter";
 
 export const appRouter = router({
-    adminCategoriesRouter
+    adminCategoriesRouter,
+    adminTicketRouter,
 });
 
 export type AppRouter = typeof appRouter;
