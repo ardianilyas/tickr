24856af78d97dc35fcc8b49b81039f105a90e40d
ardianilyas <ardianilyas@gmail.com>
2025-11-14 22:32:29 +0700

    feat(ui): update not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,18 @@
+"use client"
+
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 export default function NotFoundPage() {
+    const searchParams = useSearchParams();
+    const message = searchParams.get("message");
+
     return (
         <div className="flex flex-col min-h-screen items-center justify-center bg-fuchsia-50 font-sans dark:bg-black">
             <h1 className="text-3xl font-medium">404 - Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
+            <p>
+                {message ?? "The page you are looking for does not exist."}
+            </p>
             <Link href="/dashboard">Go Home</Link>
         </div>
     )
